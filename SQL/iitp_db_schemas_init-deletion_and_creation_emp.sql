03f52fbed16ce1a5b9595bcd51a26452daeb9315
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-24 11:10:23 +0900

    [Add] 1. sys_common_code 에 faq_type, qna_type group 추가   2. sys_admin_account, sys_faq_info 초기 데이터 추가

--- SQL/iitp_db_schemas_init-deletion_and_creation_emp.sql
+++ SQL/iitp_db_schemas_init-deletion_and_creation_emp.sql
@@ -1,5 +1,5 @@
 -- ## iitp DB Schemas - Initial setup - Creation and Delete if tables exists
--- ## ver 0.0.4 last update data : 2025.07.11
+-- ## ver 0.0.4 last updated data : 2025.07.11
 -- ## Only for PostgreSQL
 -- ## 고용관련 정적 데이터용 테이블 생성 스크립트
 -- ## 나라 HR 데이터 제공 데이터
