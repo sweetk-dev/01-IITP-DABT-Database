1bbfa1efef75b22380add9aa6f26d5a90b627d77
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-25 20:46:59 +0900

    [Change] sys_adm_account 초기 설정 변경

--- SQL/iitp_db_schemas_init-Required_data_insert-Sys.sql
+++ SQL/iitp_db_schemas_init-Required_data_insert-Sys.sql
@@ -1,6 +1,6 @@
 
 -- ## iitp DB Schemas - Initial setup - Insert Data for Required System Data
--- ## ver 0.1.1 last updated data : 2025.07.24
+-- ## ver 0.1.2 last updated data : 2025.07.25
 -- ## Only for PostgreSQL
 -- ## 초기 데이터 삽입 : 공통 코드 및 시스템 필수 데이터 
 
@@ -195,7 +195,7 @@ INSERT INTO public.sys_common_code (grp_id, grp_nm, code_id, code_nm, parent_grp
 -- 최초 로그인시 비밀번호 변경하도록 되어 있음!! 초기 비밀번호 - 12345!!!
 TRUNCATE TABLE  public.sys_adm_account;
 INSERT INTO public.sys_adm_account (login_id, "password", "name", roles, status, del_yn, affiliation, description, note, created_at, created_by) 
-			VALUES('sweetkRoot01', '$2b$10$JFa.OPm59QwgISNqTqMu/.B3fGcAZIxPccJr40FIHRR9Oug8ouvbK', 'sweetk-root-01', 'S-Admin', 'A'::bpchar, 'N'::bpchar, 'sweetk', 'sweetk super admin', '', CURRENT_TIMESTAMP, 'SYS-MANUAL');
+			VALUES('sweetkRoot01@sweetk.co.kr', '$2b$10$JFa.OPm59QwgISNqTqMu/.B3fGcAZIxPccJr40FIHRR9Oug8ouvbK', 'sweetk-root-01', 'S-Admin', 'A'::bpchar, 'N'::bpchar, 'sweetk', 'sweetk super admin', '', CURRENT_TIMESTAMP, 'SYS-MANUAL');
 
 
 
